8a3e54ab76698a8271e68093f14039f335024b3e
Abdulloh2010-start <abdullohlalala@gmail.com>
2025-09-10 22:22:00 +0500

    AniLife_tv

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,9 @@ import { ThemeProvider } from "./components/ThemeContext";
 import { UserProvider } from "./contexts/UserContext";
 import NotificationsProvider from './contexts/NotificationsProvider';
 import { HelmetProvider } from "@dr.pogodin/react-helmet";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({immediate: true});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
+
+const manifest = {"theme_color":"#8936FF","background_color":"#2EC6FE","icons":[{"purpose":"maskable","sizes":"512x512","src":"icon512_maskable.png","type":"image/png"},{"purpose":"any","sizes":"512x512","src":"icon512_rounded.png","type":"image/png"}],"orientation":"any","display":"standalone","dir":"auto","lang":"ru","name":"AniLife_tv","short_name":"Anime hosting","description":"in this app you can watch new animes"};
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{html,css,js,ico,png,svg}"],
+      },
+      manifest: manifest,
+    })
+  ],
 });
