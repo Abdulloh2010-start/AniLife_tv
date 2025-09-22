1c26cce136c08f35e2f0255cfa1d7066f2b37abc
Abdulloh2010-start <abdullohlalala@gmail.com>
2025-09-22 15:58:44 +0500

    set telegram web app

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeContext";
 import { UserProvider } from "./contexts/UserContext";
-import NotificationsProvider from './contexts/NotificationsProvider';
+import NotificationsProvider from "./contexts/NotificationsProvider";
 import { HelmetProvider } from "@dr.pogodin/react-helmet";
 import { registerSW } from "virtual:pwa-register";
 
-registerSW({immediate: true});
+registerSW({ immediate: true });
+
+function TelegramProvider({ children }) {
+  useEffect(() => {
+    if (window.Telegram?.WebApp) {
+      const tg = window.Telegram.WebApp;
+      tg.ready();
+      tg.expand();
+      tg.MainButton.setText("Отправить в бота");
+      tg.MainButton.onClick(() => {
+        tg.sendData(JSON.stringify({ action: "hello_from_webapp", time: Date.now() }));
+      });
+    }
+  }, []);
+
+  return children;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -17,7 +33,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <ThemeProvider>
           <UserProvider>
             <NotificationsProvider>
-              <App />
+              <TelegramProvider>
+                <App />
+              </TelegramProvider>
             </NotificationsProvider>
           </UserProvider>
         </ThemeProvider>
